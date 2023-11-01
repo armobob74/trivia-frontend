2a9632d39ac2a2dfaebbe211edb6e5e35d4eddff
Armon <armon@nudgex.net>
2023-10-31 18:14:32 -0700

    switched from custom tailwind to using SkeletonUI defaults

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,16 @@
 <script>
-	import Link from '$lib/Link.svelte';
-	import LinkPrimary from '$lib/LinkPrimary.svelte';
+	//import Link from '$lib/Link.svelte';
 	let game_id = '';
 </script>
 
 <a class="btn absolute top-1 right-1" href={`/create-game/`}>Create Game</a>
 <div class="space-y-5 flex flex-col justify-center items-center h-full mx-auto">
-	<form class="shadow bg-gray-800 flex flex-col p-8 rounded-xl">
+	<form class="bg-slate-300 dark:bg-gray-800 flex flex-col p-8 rounded-xl">
 		<div class="mb-4">
-			<label for="game-id">Game ID:</label>
+			<label class="label" for="game-id">Game ID:</label>
 			<input
-				class="bg-white border border-white dark:bg-purple-500 rounded p-1"
+				placeholder="1234..."
+				class="input text-center"
 				type="text"
 				name="game-id"
 				id="game-id"
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import { join } from 'path'
 import type { Config } from 'tailwindcss'
 import typography from '@tailwindcss/typography';
+import forms from '@tailwindcss/forms';
 import { skeleton } from '@skeletonlabs/tw-plugin'
 
 export default {
@@ -10,6 +11,7 @@ export default {
 		extend: {},
 	},
 	plugins: [
+		forms,
 		typography,
 		skeleton({
 			themes: {
