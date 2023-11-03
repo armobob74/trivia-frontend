3135ed9863897bc890ec7c53278c2a38591df301
Armon <armon@nudgex.net>
2023-11-02 23:51:29 -0700

    question generator now includes categories

--- generating_questions/generate_prompt.py
+++ generating_questions/generate_prompt.py
@@ -1,9 +1,14 @@
 import json
+import pdb
 import random
 from get_unfilled_questions import getUnfilledQuestions
 
 with open('./questions_ready.json') as f:
     questions_ready = json.load(f)
+
+with open('./categories.json') as f:
+    categories = json.load(f)
+    
 
 # this function finds only the questions that have not been filled by chatgpt
 questions_unready = getUnfilledQuestions()
@@ -17,11 +22,16 @@ sample_unready = questions_unready[:20]
 text_ready = json.dumps(sample_ready, indent=1)
 text_unready = json.dumps(sample_unready, indent=1)
 
-prompt = f"""I am working on a database of trivia questions. I have a large number of question/answer pairs, but I need to generate alternative answers as well. I want the questions to look like this when done:
-{text_ready}
+prompt = f"""I am working on a database of trivia questions. I have a large number of question/answer pairs, but I need you to add some information.
 
 Here are my unfinished questions:
 {text_unready}
-Please fill out the alternate answers for each unfinished question, changing NOTHING ELSE."""
+
+They are meant to be multiple choice, with answers A-D. Please add in the missing answers, and then add a categories field with a list of appropriate categories for the question.
+
+Here is the list of categories:
+{categories}
+
+Change NOTHING ELSE."""
 print(prompt)
 
--- generating_questions/readme.md
+++ generating_questions/readme.md
@@ -2,7 +2,7 @@
 scripts to help with asking chatgpt to fill out a questions database
 
 generate\_prompt.py will generate a prompt for chatgpt
-the generated responses must be stored in questions\_chatgpt\_filled.json
+the generated responses must be stored in questions\_chatgpt\_filled.json, but not removed from the original list.
 
 `make_sure_no_changes.py` will double-check chatgpt's work.
 It's ok for very minor adulturations to exist, as long as it's nothing that changes the meaning of the question.
